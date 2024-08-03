a8e8a5ba5ed1e44237f027f634b73ea6e2463b63
TBKaumadhi <kawmadhithrikawala@gmail.com>
2024-08-03 20:50:39 +0530

    password reset service

--- src/edu/lms/service/custom/impl/ResetServiceImpl.java
+++ src/edu/lms/service/custom/impl/ResetServiceImpl.java
@@ -6,8 +6,9 @@ package edu.lms.service.custom.impl;
 
 import edu.lms.dao.DaoFactory;
 import edu.lms.dao.custom.ResetDao;
-import edu.lms.dto.UserDto;
-import edu.lms.entity.UserEntity;
+import edu.lms.dto.RegisterDto;
+import edu.lms.entity.RegisterEntity;
+
 import edu.lms.service.custom.ResetService;
 
 /**
@@ -18,17 +19,26 @@ public class ResetServiceImpl implements ResetService{
     private ResetDao resetDao = (ResetDao)DaoFactory.getInstance().getDao(DaoFactory.DaoType.RESET);
 
     @Override
-    public UserDto get(String userName) throws Exception {
-        UserEntity userEntity= resetDao.get(userName);
+    public RegisterDto get(String userName) throws Exception {
+        RegisterEntity registerEntity= resetDao.get(userName);
         
-        if(userEntity != null){
-            return new UserDto(userEntity.getUserName(),userEntity.getSecQ(),userEntity.getAnswer(),userEntity.getPassword());
+        if(registerEntity != null){
+            RegisterDto dto= new RegisterDto();
+            dto.setAnswer(registerEntity.getAnswer());
+            dto.setSecQuestion(registerEntity.getSecQuestion());
+            dto.setUserName(registerEntity.getUserName());
+            dto.setPassword(registerEntity.getPassword());
+            
+            return dto;
         }return null;
     }
 
     @Override
-    public String update(UserDto userDto) throws Exception {
-        UserEntity entity = new UserEntity(userDto.getUserName(),userDto.getSecQ(),userDto.getAnswer(),userDto.getPassword());
+    public String update(RegisterDto registerDto) throws Exception {
+        RegisterEntity entity = new RegisterEntity();
+        entity.setUserName(registerDto.getUserName());
+        entity.setPassword(registerDto.getPassword());
+        
         return resetDao.update(entity)? "Success": "Fail" ;
                
     }
